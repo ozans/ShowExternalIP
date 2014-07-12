c83bcdb5b3db5dc3e597390d3cd9fe3c273db8ad
Ozan Safi <ozan@oscorp.se>
2014-07-12 13:08:58 +0200

    correct indentation for the whole file
    Signed-off-by: Ozan Safi <ozan@oscorp.se>

--- src/com/ozan/showexternalip/MainActivity.java
+++ src/com/ozan/showexternalip/MainActivity.java
@@ -24,63 +24,63 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		finish();
-		
+
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
-		        try {
-		        	String URL="http://ipecho.net/plain";
-		        	HttpClient httpclient = new DefaultHttpClient();
-		        	HttpResponse response = httpclient.execute(new HttpGet(URL));
-		        	final StatusLine statusLine = response.getStatusLine();
-		        	if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-		        		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		        		response.getEntity().writeTo(out);
-		        		out.close();
-		        		final String responseString = out.toString();
+				try {
+					String URL="http://ipecho.net/plain";
+					HttpClient httpclient = new DefaultHttpClient();
+					HttpResponse response = httpclient.execute(new HttpGet(URL));
+					final StatusLine statusLine = response.getStatusLine();
+					if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+						ByteArrayOutputStream out = new ByteArrayOutputStream();
+						response.getEntity().writeTo(out);
+						out.close();
+						final String responseString = out.toString();
 
-		        		InetAddress ia = InetAddress.getByName(responseString);
-		        		final String hostname=ia.getCanonicalHostName();
+						InetAddress ia = InetAddress.getByName(responseString);
+						final String hostname=ia.getCanonicalHostName();
 
-		        		WifiManager manager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-		        		WifiInfo info = manager.getConnectionInfo();
+						WifiManager manager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+						WifiInfo info = manager.getConnectionInfo();
 
-		        		final String macaddress = info.getMacAddress();
-		        		runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                            	String str="External IP Address: " + responseString + "\n" + "Hostname: " + hostname;
-                            	if (macaddress != null) {
-                            		str+="\n" + "WiFi MAC Address: " + macaddress;
-                            	}
+						final String macaddress = info.getMacAddress();
+						runOnUiThread(new Runnable() {
+							@Override
+							public void run() {
+								String str="External IP Address: " + responseString + "\n" + "Hostname: " + hostname;
+								if (macaddress != null) {
+									str+="\n" + "WiFi MAC Address: " + macaddress;
+								}
 
-                            	Toast.makeText(getBaseContext(), str, Toast.LENGTH_LONG).show();
-                           }
-                       });
-		        	} else{
-		        		response.getEntity().getContent().close();
-		        		runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(getBaseContext(), "Error: " + statusLine.getReasonPhrase(),
-                      	              Toast.LENGTH_LONG).show();
-                           }
-                       });
-		        		throw new IOException(statusLine.getReasonPhrase());
-		        	}
-		        }
-		        catch (final Exception e)
-		        {
-		         	runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                        	Toast.makeText(getBaseContext(), e.toString(),
-                    	              Toast.LENGTH_LONG).show();
-                       }
-                   });
-		        }
+								Toast.makeText(getBaseContext(), str, Toast.LENGTH_LONG).show();
+							}
+						});
+					} else{
+						response.getEntity().getContent().close();
+						runOnUiThread(new Runnable() {
+							@Override
+							public void run() {
+								Toast.makeText(getBaseContext(), "Error: " + statusLine.getReasonPhrase(),
+										Toast.LENGTH_LONG).show();
+							}
+						});
+						throw new IOException(statusLine.getReasonPhrase());
+					}
+				}
+				catch (final Exception e)
+				{
+					runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							Toast.makeText(getBaseContext(), e.toString(),
+									Toast.LENGTH_LONG).show();
+						}
+					});
+				}
 			}
-		 }).start();
+		}).start();
 	}
 
 }
